2753cccf6c2397f051b2c7850b04a2f2a7c608bf
Yuri Marques <yuriifap@gmail.com>
2026-09-21 16:54:23 -0300

    docs: adiciona id em Patch para atualização e mostrar idade

--- aula08-09-metodo-get-post-put-patch-delete/src/convidados.controller.ts
+++ aula08-09-metodo-get-post-put-patch-delete/src/convidados.controller.ts
@@ -20,7 +20,7 @@ export class ConvidadosController {
             }
         }
 
-        @Patch()
+        @Patch(':id')
         atualizarIdade(@Param('id') id: number, @Body('idade') idade: number){
             console.log(`[ADMINISTRADOR]: Atualizando a idade do ID ${id}`);
             return this.convidadosService.atualizarIdade(+id, idade);
