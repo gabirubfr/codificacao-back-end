d76819db126834f4c9679f6ee01969f152ddace8
Yuri Marques <yuriifap@gmail.com>
2026-09-23 17:18:21 -0300

    feat: cria MidiaController com upload de arquivos, renomeacao por UUID e validacoes

---
+++ aula11-api-fluxo-imagem-midia/src/midia.controller.ts
@@ -0,0 +1,44 @@
+import { Controller, Post, UseInterceptors, UploadedFile, BadRequestException, Inject } from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
+import { extname } from "path";
+import { fileURLToPath } from "url";
+
+@Controller('midia')
+export class MidiaController {
+    @Post('upload')
+    @UseInterceptors(
+        FileInterceptor('arquivo',{
+            storage: diskStorage ({
+                destination: './uploads',
+                filename: (req, file, calkback) =>{
+                    const nomeUnico = `${uuidv4()}${extname(file.originalname)}`;
+                    calkback (null, nomeUnico);
+                },
+            }),
+            limits: {
+                fileSize: 2 * 1024 * 1024,
+            },
+            fileFilter: (req, file, callback) =>{
+                if(!file.mimetype.match(/\/(jpg|jpeg|png|gif|webp)$/)){
+                    return callback(
+                        new BadRequestException('Apenas arquivos do tipo: jpg, jpeg, png, gif e webp'),
+                        false,
+                    );
+                }
+                callback(null, true);
+            },
+        })
+    )
+    uploadedFile(@UploadedFile() file: Express.Multer.File){
+        if(!file){
+            throw new BadRequestException('Nenhum arquivo enviado')
+        }
+        return {
+            filename: file.fieldname,
+            size: file.size,
+            url: `http://localhost:3000/api/uploads/${file.filename}`,
+        };
+    }
+}
